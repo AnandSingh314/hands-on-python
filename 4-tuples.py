4919fc5bb303461aa6a764e6a30d041a0939bb37
Anand Vijay <avsmips@gmail.com>
2018-05-19 07:55:01 +0530

    added python3 syntax.

--- 4-tuples.py
+++ 4-tuples.py
@@ -4,11 +4,11 @@ tuple3 = 1, 2, 3, 4  # parenthesis are optional
 tuple4 = 3,  # single item tuple
 tuple5 = tuple(['one', 'two', 'three', 'four'])  # tuple from list
 
-print tuple1
-print tuple2
-print tuple3
-print tuple4
-print tuple5
+print (tuple1)
+print (tuple2)
+print (tuple3)
+print (tuple4)
+print (tuple5)
 
 # tuple is immutable, below line show error while modification.
 # del(tuple3[1])
@@ -16,10 +16,10 @@ print tuple5
 
 tuple6 = ([1,2,3,],10)
 tuple6[0][1]=20
-print tuple6
+print (tuple6)
 del(tuple6[0][1])
-print tuple6
+print (tuple6)
 
 tuple7 = 1,2,3,4,5,6,7,8,9
-print 3 in tuple7
-print 3 not in tuple7
+print (3 in tuple7)
+print (3 not in tuple7)
--- 6-dict.py
+++ 6-dict.py
@@ -1,40 +1,40 @@
 dict1 = {'name': 'Anand', 'addr': 'Bangalore', 'age': 26}
-print dict1
+print (dict1)
 
 dict2 = dict([('name', 'Anand'), ('addr', 'bangalore'), ('age', 21)])
-print dict2
+print (dict2)
 
 dict3 = dict(name='Anand', addr='bangalore', age=24)
-print dict3
+print (dict3)
 
 # baisc dict operation
 
 dict2['addr'] = 'Bangalore'
-print dict2
+print (dict2)
 
-print len(dict2)
+print (len(dict2))
 
-print 'addr' in dict2  # looks for key
+print ('addr' in dict2)  # looks for key
 
-print 'addr' not in dict2
+print ('addr' not in dict2)
 
 dict3.clear()
-print dict3
+print (dict3)
 
 del dict3
 
 # accessing key & values in dict
-print dict1.keys()
-print dict1.values()
-print dict1.items()
+print (dict1.keys())
+print (dict1.values())
+print (dict1.items())
 a, b, c = dict1.items()
-print a, b, c
-print 'Anand' in dict1.values()
+print (a, b, c)
+print ('Anand' in dict1.values())
 
 # iterating a dict
 
 for item in dict1:
-    print item, dict1[item]
+    print (item, dict1[item])
 
 for key, val in dict1.items():
-    print (key, val)
+    print ((key, val))
